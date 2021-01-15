5e2d8af1d648f3125ed7ce84950de575d819634f
MariekePas <70317441+MariekePas@users.noreply.github.com>
2021-01-15 07:36:43 +0100

    first commit

---
+++ Index/Javascript/weather.js
@@ -0,0 +1,15 @@
+function weatherBalloon( cityID ) {
+    var key = '{b5603cee5a6da3ca766256a057d7ec63}';
+    fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID+ '&appid=' + key)  
+    .then(function(resp) { return resp.json() }) // Convert data to json
+    .then(function(data) {
+      console.log(data);
+    })
+    .catch(function() {
+      // catch any errors
+    });
+  }
+  
+  window.onload = function() {
+    weatherBalloon( 6167865 );
+  }
